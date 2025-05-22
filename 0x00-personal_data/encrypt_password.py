1574bd51867d3cb3da166fc0af454342edd006de
dohaehab <dohaehab@gmail.com>
2025-05-23 01:09:51 +0300

    task 0

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+function hash_password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    :param password: plain text
+    :return: hashed password
+    """
+    encoded_pass = password.encode('UTF-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(encoded_pass, salt)
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """
+    :param hashed_password: hashed pass
+    :param password: plain password
+    :return: True or False
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
