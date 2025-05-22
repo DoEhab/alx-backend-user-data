739ae5a1d00ec6164f182f7ea8ac37ae3711e16c
dohaehab <dohaehab@gmail.com>
2025-05-23 00:28:06 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,16 +16,16 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, field: List[str]):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.field = field
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
         format and redact
         :return: redacted message
         """
-        record.msg = filter_datum(self.field, self.REDACTION,
+        record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
 
