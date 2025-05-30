0964293f55d0ea9a3ea880dbaac93223e2df8799
dohaehab <dohaehab@gmail.com>
2025-05-31 01:00:23 +0300

    task 0

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - API authorization
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - API authorization
+    """
+    abort(403)
