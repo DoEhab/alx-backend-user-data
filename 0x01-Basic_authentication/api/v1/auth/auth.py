eab0cc4b5bd78ca82954c1e428c3ce53c0ba63f1
dohaehab <dohaehab@gmail.com>
2025-05-31 01:41:16 +0300

    task 0

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,6 +16,13 @@ class Auth:
         """
         :return: False
         """
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if path in excluded_paths:
+            return False
+
         return False
 
     def authorization_header(self, request=None) -> str:
