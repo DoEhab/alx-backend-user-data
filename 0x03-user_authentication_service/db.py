72b0696d5441f9266a1525819ad0c84a871db178
dohaehab <dohaehab@gmail.com>
2025-06-05 19:02:00 +0300

    task 0

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,7 +35,7 @@ class DB:
         :param hashed_password: required pass
         :return: new user
         """
-        new_user = User(email, hashed_password)
+        new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
