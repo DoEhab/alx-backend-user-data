1df11e38328126f8fc9dc15ee0515e68ff1f51f3
dohaehab <dohaehab@gmail.com>
2025-06-05 18:24:43 +0300

    task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+"""
+class User for data mode
+"""
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Define table users
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=False)
+    reset_token = Column(String, nullable=False)
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
+            self.name, self.fullname, self.nickname)
