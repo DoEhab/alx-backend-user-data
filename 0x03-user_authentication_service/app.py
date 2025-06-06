b8720eb13ccaecfb1a824a5d78859c5c67916668
dohaehab <dohaehab@gmail.com>
2025-06-07 02:00:34 +0300

    task 1

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,16 +17,14 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=['POST'])
+@app.route("/users", methods=["POST"])
 def users():
-    """ register a user
-    """
     email = request.form.get("email")
     password = request.form.get("password")
 
     try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 200
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
